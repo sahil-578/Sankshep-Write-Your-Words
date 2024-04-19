1ed8ff05454dda57041fa9f2984797a39222a4fb
Sahil <sahil1372001@gmail.com>
2024-04-19 15:19:32 +0530

    Build Authentication with Appwrite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  console.log(import.meta.env)
+
+  return (
+    <>
+      <h1>Sankshep - Write Your Own </h1>
+    </>
+  )
+}
+
+export default App
